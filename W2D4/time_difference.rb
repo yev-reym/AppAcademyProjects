ff25209f78d4aa995728b18be5198d09801d6581
App Academy Student <yevgeniyreym@gmail.com>
2019-04-18 15:35:47 -0400

    Add anagram exercises

--- W2D4/time_difference.rb
+++ W2D4/time_difference.rb
@@ -41,6 +41,9 @@ end
 def largest_contiguous_subsum_2(arr)
     current_max = 0
     accumulated_max = 0
+
+    return arr.max if arr.all? { |num| num < 0 }
+
     (0...arr.length).each do |i|
         current_max += arr[i]
 
@@ -51,6 +54,51 @@ def largest_contiguous_subsum_2(arr)
     accumulated_max
 end
 
-list = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum_1(list)
-p largest_contiguous_subsum_2(list)
+# list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum_1(list)
+# p largest_contiguous_subsum_2(list)
+
+def first_anagram?(str1, str2)
+    arr1 = str1.split("")
+    anagrams = arr1.permutation.to_a
+    arr2 = str2.split("")
+    anagrams.include?(arr2)
+end
+
+def second_anagram?(str1, str2)
+    
+    str1.each_char do |char|
+        index = str2.index(char)
+        return false if index.nil?
+        str2[index] = "" 
+    end
+
+    str2.empty?
+end
+
+# p first_anagram?("gizmoadrgasef", "sallydgdfgrg")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
+
+# p second_anagram?("gizmofdfffn", "sallyxfghfgnxgn")    #=> false
+# p second_anagram?("elvis", "lives")  #=> true
+
+def third_anagram?(str1, str2)
+    sorted1 = str1.split("").sort
+    sorted2 = str2.split("").sort
+    sorted1 == sorted2
+end
+
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")  #=> true
+
+def fourth_anagram?(str1, str2)
+    return false if str1.length != str2.length
+    anagram_hash = {}
+
+    str1.each { |char| anagram }
+
+end
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")  #=> true
